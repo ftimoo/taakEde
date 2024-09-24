152bb3f56fc784a75437d16ab12a22c27b9bf39f
Timo <Timo-faes@live.be>
2024-09-24 14:17:48 +0200

    init cars

---
+++ CarsMicroService/src/main/java/com/example/carsmicroservice/CarsMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.carsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsMicroServiceApplication.class, args);
+	}
+
+}
---
+++ CarsMicroService/src/test/java/com/example/carsmicroservice/CarsMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
