e236d3ff3b5e21a399a7047d9761de7b01e93a25
Timo <Timo-faes@live.be>
2024-12-16 04:20:49 +0100

    cors

--- CarServiceApplication/src/main/java/com/example/carserviceapplication/controller/CarController.java
+++ CarServiceApplication/src/main/java/com/example/carserviceapplication/controller/CarController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/car")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class CarController {
     private final CarService carService;
 
