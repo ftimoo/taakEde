de0f5707a34a905928a09e3e3cb60d9d2d99e6b6
Timo <Timo-faes@live.be>
2024-12-17 21:34:19 +0100

    stupid mistake

--- gateway/src/main/java/com/example/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/example/gateway/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity
                 .authorizeExchange(exchange ->
-                        exchange.pathMatchers(HttpMethod.GET,"/products")
+                        exchange.pathMatchers(HttpMethod.GET,"/car")
                                 .permitAll()
                                 .anyExchange()
                                 .authenticated()
