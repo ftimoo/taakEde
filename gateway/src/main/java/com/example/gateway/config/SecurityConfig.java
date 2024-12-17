075155da37bfc6a589e49242b0a8292c96c27000
Timo <Timo-faes@live.be>
2024-12-17 21:57:10 +0100

    car to cars

--- gateway/src/main/java/com/example/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/example/gateway/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity
                 .authorizeExchange(exchange ->
-                        exchange.pathMatchers(HttpMethod.GET,"/car")
+                        exchange.pathMatchers(HttpMethod.GET,"/cars")
                                 .permitAll()
                                 .anyExchange()
                                 .authenticated()
